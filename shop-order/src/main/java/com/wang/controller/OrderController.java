bb5208e0b6c4a2fd23589807b52442fe1f15ee04
wangguangpeng123 <598970984@qq.com>
2022-03-21 23:11:24 +0800

    增加sentinel服务

--- shop-order/src/main/java/com/wang/controller/OrderController.java
+++ shop-order/src/main/java/com/wang/controller/OrderController.java
@@ -9,14 +9,14 @@ import com.wang.common.pojo.User;
 import com.wang.server.ProductService;
 import com.wang.services.OrderService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -40,6 +40,15 @@ public class OrderController {
 
         Product product = productService.findByPid(pid);
         log.info(">>商品信息，查询结果：{}", product.toString());
+
+        // TODO 模拟一次网络延迟
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //创建订单
         Order order = new Order();
         order.setUid(1);
         order.setUsername("王发财");
@@ -47,12 +56,28 @@ public class OrderController {
         order.setPprice(product.getPprice());
         order.setPname(product.getPname());
         order.setNumber(1);
-        orderService.save(order);
+//        orderService.save(order);
 
         Gson gson = new Gson();
         Map<String, Object> result = gson.fromJson(gson.toJson(order), new TypeToken<Map<String, Object>>() {}.getType());
         result.putAll(gson.fromJson(gson.toJson(product), new TypeToken<Map<String, Object>>() {}.getType()));
+        log.info("创建订单成功，订单信息为{}", gson.toJson(result));
         return result;
+    }
+
+    @RequestMapping("/order/message")
+    public String message(){
+        return "高并发下的问题test";
+    }
+
+    @RequestMapping("/order/message1")
+    public String message1(){
+        return "message1";
+    }
+
+    @RequestMapping("/order/message2")
+    public String message2(){
+        return "message2";
     }
 
     @GetMapping("error")
