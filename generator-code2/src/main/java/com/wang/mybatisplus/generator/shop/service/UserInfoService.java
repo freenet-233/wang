5fe74c5b8430ba5f11887d561dca2bfa3657d08f
wangguangpeng <wangguangpeng@tech-winning.com>
2022-05-21 12:31:58 +0800

    Generate Code Module Test：user_info

---
+++ generator-code2/src/main/java/com/wang/mybatisplus/generator/shop/service/UserInfoService.java
@@ -0,0 +1,13 @@
+package com.wang.mybatisplus.generator.shop.service;
+
+import com.wang.mybatisplus.generator.shop.domain.UserInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author wangguangpeng
+* @description 针对表【user_info】的数据库操作Service
+* @createDate 2022-05-20 23:05:37
+*/
+public interface UserInfoService extends IService<UserInfo> {
+
+}
---
+++ generator-code2/src/main/java/com/wang/mybatisplus/generator/shop/service/impl/UserInfoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.wang.mybatisplus.generator.shop.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wang.mybatisplus.generator.shop.domain.UserInfo;
+import com.wang.mybatisplus.generator.shop.service.UserInfoService;
+import com.wang.mybatisplus.generator.shop.mapper.UserInfoMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author wangguangpeng
+* @description 针对表【user_info】的数据库操作Service实现
+* @createDate 2022-05-20 23:05:37
+*/
+@Service
+public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements UserInfoService{
+
+}
+
+
+
+
