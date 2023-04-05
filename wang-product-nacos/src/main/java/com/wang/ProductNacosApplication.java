4f76767d4fdf115a9b1994a69e8c7cacbc2aa1d0
Robin Wang <598970984@qq.com>
2023-04-05 22:32:21 +0800

    add nacos config.

--- wang-product-nacos/src/main/java/com/wang/ProductNacosApplication.java
+++ wang-product-nacos/src/main/java/com/wang/ProductNacosApplication.java
@@ -2,13 +2,14 @@ package com.wang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 /**
  * @author wangguangpeng
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductNacosApplication {
     public static void main( String[] args ){
         SpringApplication.run(ProductNacosApplication.class, args);
