c1db05a0b8a413fe60d9407e730dbcfb658abb57
Robin Wang <598970984@qq.com>
2023-04-20 01:31:43 +0800

    Update spring security project.

--- wang-security/src/main/java/com/wang/auth/AuthenticationService.java
+++ wang-security/src/main/java/com/wang/auth/AuthenticationService.java
@@ -106,7 +106,7 @@ public class AuthenticationService {
         if (userEmail != null) {
             var user = this.repository.findByEmail(userEmail)
                     .orElseThrow();
-            if (jwtService.isTokenValid(refreshToken, user)) {
+            if (Boolean.TRUE.equals(jwtService.isTokenValid(refreshToken, user))) {
                 var accessToken = jwtService.generateToken(user);
                 revokeAllUserTokens(user);
                 saveUserToken(user, accessToken);
--- wang-security/src/main/java/com/wang/config/SecurityConfig.java
+++ wang-security/src/main/java/com/wang/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
             .authorizeHttpRequests()
             .requestMatchers("/api/v1/auth/**")
             .permitAll()
+            .requestMatchers("/static/**", "/resources/**").permitAll()
             .anyRequest()
             .authenticated()
             .and()
